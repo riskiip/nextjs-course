7b757ba9024fc6b67b123156f124621adb2145c8
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-01-08 07:27:33 +0700

    feat : Learn file colocation

---
+++ src/app/profile/admin.tsx
@@ -0,0 +1,5 @@
+export default function Admin({name} : {
+  name: string
+}) {
+  return <h2>Ini halaman Admin {name}</h2>
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,10 @@
+import Admin from "./admin";
+
 export default function Profile() {
-  return <h1>Ini halaman Profile</h1>
+  return (
+    <div>
+      <h1>Ini halaman profile</h1>
+      <Admin name="rizki"></Admin>
+    </div>
+  );
 }

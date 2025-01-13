f00b4e87afc2143c96a903afd2c6c8c830ec8b87
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-01-13 20:28:46 +0700

    feat: Learn generate dynamic metadata

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,18 @@
-export default function ProductDetails({
-  params,
-}: {
-  params: { productsId: string };
-}) {
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string
+  }
+}
+
+export const generateMetadata = ({params}: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`
+  }
+}
+
+export default function ProductDetails({params}: Props) {
   // productId bisa otomatis menjadi dynamic params karena nama folder (file-system based)
-  return <h1>Ini detail produk {params.productsId}</h1>;
+  return <h1>Ini detail produk {params.productId}</h1>;
 }

beff20fcff902327c8033989de6b12397e0a37ef
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2025-01-16 22:14:00 +0700

    feat : Learn active link (active route)

--- src/app/(auth)/(auth-layout)/layout.tsx
+++ src/app/(auth)/(auth-layout)/layout.tsx
@@ -1,12 +1,30 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
+
+const links = [
+  {name: 'Register', href: '/register'},
+  {name: 'Login', href: '/login'}
+]
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathName = usePathname();
+
   return (
     <section>
-      <h2>Ini layout untuk regis dan login</h2>
-      {children}
+      <div>
+        {links.map((link) => {
+          const isActive = pathName.startsWith(link.href);
+          return (
+            <Link href={link.href} key={link.name} className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>{link.name}</Link>
+          )
+        })}
+      </div>
     </section>
   );
 }
